513116f3b7a1e05fa35fda6e5c6538817b98b079
ahmelq <ahmed.elq53@gmail.com>
2019-02-24 19:10:50 +0300

    implemented itrv. insertion sort alg.

---
+++ sorting/insertionsort.py
@@ -0,0 +1,16 @@
+#Author: ahmelq -  github.com/ahmedelq
+#License: MIT
+
+def iss(ary):
+    ary_len = len(ary)
+    for i in range(ary_len):
+        j = i
+        while ary[j] < ary[j - 1] and j > 0:
+            ary[j], ary[j - 1] = ary[j - 1], ary[j]
+            j -= 1
+    return ary
+
+if __name__ == "__main__":
+    ary = [5, 6, 2, 4, 1, 3, 1]
+    print("Array before:", ary)
+    print("Insertion sort:", iss(ary))
