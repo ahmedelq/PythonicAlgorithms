3ebf46df3a7eda28c833524283ede0318ae797dc
ahmedelq <ahmed.elq53@gmail.com>
2019-03-03 17:42:43 +0300

    added shift chiper

--- fun/cc.py
+++ fun/cc.py
@@ -1,12 +1,20 @@
-
-def main(txt):
-    start = 97
-    end = 122
-    mod = 122 - 97 + 1
-
-    shft_amt = 3
-    print( chr(ord(txt)) + shft_amt)
+#Author: ahmelq -  github.com/ahmedelq/
+#License: MIT
+#This is a solution of the challange: https://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/
+def sc(txt, shft_amt=3, isDeq=False):
+    """This is a chiper alg; given a text an a possible integer, it will shift the text 'shft_amt' """
+    shft_amt *= (-1) ** isDeq
+    result = ""
+    for c in txt:
+        new_chr = c
+        if c.isalpha():
+            ascii_chr = ord(c)
+            new_ascii = ( (ascii_chr & 0x1F) + shft_amt ) % 26
+            new_chr   = chr(new_ascii | ascii_chr & 96)
+        result += new_chr
+    return result
 
 
 if __name__ == "__main__":
-    main(txt)
+    txt = "Attack Mars at 13:37"
+    print(sc(txt), "-->", sc(sc(txt), 3, True) )
