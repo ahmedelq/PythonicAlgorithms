740653e9d372a3b4dfa391c483a2482fd047fed7
ahmedelq <ahmed.elq53@gmail.com>
2020-05-15 15:03:33 +0300

    bs

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -3,12 +3,12 @@
 
 def bubble_sort(ary):
     """Sorts an array in a nondecreasing order""" 
-    for i in range(len(ary)):
-        for j in range(len(ary) - 1):
+    for i in range(len(ary), -1, -1):
+        for j in range(i - 1):
             if ary[j] > ary[j + 1]: ary[j + 1], ary[j] = ary[j], ary[j + 1]
     return ary
 
 if __name__ == "__main__":
-    ary = [5, 6, 2, 4, 1, 3, 1]
-    print("Array before:", ary)
-    print("Insertion sort:", bubble_sort(ary))
+    ary = [5, 6, 2, 7, 0, 4, 1, 3, 0, 1, 0]
+    print("Before sorting:", ary)
+    print("Bubble sort:", bubble_sort(ary))
